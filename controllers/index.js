2cf22936fc429a42db3581d56765c531f6d1e1be
GroverAshish <91431919+GroverAshish@users.noreply.github.com>
2022-02-15 21:30:48 +0530

    Update index.js

--- controllers/index.js
+++ controllers/index.js
@@ -1,4 +1,5 @@
 const service = require("../Services/index")
+const message = require("../const/index")
 
 // Validation
 const sip = async (req, res) => {
@@ -6,15 +7,13 @@ const sip = async (req, res) => {
 
   //  Check If all inputs are numerical or not
   // Check if all inputs are more than zero or not
-  if (isNaN(data.period) || isNaN(data.rate) || isNaN(data.monInvest)) {
-    res.status(400).send("Invalid Inputs")
+  if ((isNaN(data.period) || isNaN(data.rate) || isNaN(data.monInvest)) || (data.period <= 0 || data.rate <= 0 || data.monInvest <= 0)) {
+    res.status(200).send(message.check.failed)
   } 
-  else if (data.period <= 0 || data.rate <= 0 || data.monInvest <= 0) {
-    res.status(400).send("Invalid Inputs")
-  } 
-  else 
-  res.status(200).send(await service.sip_ctr(data))
-  
+  else{
+    const Result = await service.sip_ctr(data);
+    res.status(200).send(Result);
+  }
 }
 
 module.exports = {
